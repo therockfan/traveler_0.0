14353df1f67c539511a4d145bea668e8e40e1a10
therockfan <kimkhun2323@gmail.com>
2026-03-13 22:07:33 +0900

    Update main.js

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ this.load.image(
 
 this.load.image(
 'enemy',
-'https://labs.phaser.io/assets/sprites/robot.png'
+'https://labs.phaser.io/assets/animations/mummy37x45.png'
 )
 
 this.load.image(
@@ -214,3 +214,4 @@ player.setFillStyle(0xff0000)
 player.body.setVelocity(0,0)
 
 }
+
